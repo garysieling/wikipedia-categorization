983110ff920e057a35178659d143b03942fd9d7a
Gary Sieling <garysieling@Garys-MBP.home>
2018-09-18 21:42:48 -0400

    More data

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let dns = require('dns'),
+    dnscache = require('dnscache')({
+        "enable" : true,
+        "ttl" : 3000,
+        "cachesize" : 10000
+    });
 let https = require('https');
 let _ = require('lodash');
 let fs = require('fs');
@@ -6,8 +12,39 @@ let mkdirp = require('mkdirp');
 let start = 'Main_topic_classifications'
 let url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:' + start + '&cmlimit=500'
 
-function get(url, path, type, cb) {
-  console.log(url);
+function tryParse(text) {
+  try {
+    return JSON.parse(text);
+  } catch(e) {
+    return '';
+  }
+}
+
+function get(depth, url, path, type, retries, cb) {
+  try { 
+    console.log(url);
+    const destFilename = path + '/' + type + '.json';
+
+    if (fs.existsSync(destFilename)) {
+      const json = JSON.parse(fs.readFileSync(destFilename, 'utf-8'));
+      let more = 
+        json.query.categorymembers.map(
+          (cat) => cat.title
+        ).map(
+          (title) => 
+            _.partial(
+              get,
+              depth + 1,
+              'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=' + title + '&cmlimit=500&cmtype=subcat',
+              path + '/' + title.split(':')[1],
+              'categories',
+              5
+            )
+          );
+
+      return cb(more);
+    }
+     
   https.get(
     url,
     (res) => {
@@ -18,52 +55,82 @@ function get(url, path, type, cb) {
       });
 
       res.on('end', function(){
-        let json = JSON.parse(body);
+        console.log('*' + body + '*');
+        let json = tryParse(body);
        
+        if (body.length === 0 || _.get(json, ['query', 'categorymembers'], '') === '') {
+          if (retries > 0) {
+            return cb([_.partial(get, depth, url, path, type, retries - 1)]);
+          } else {
+            return cb([]);
+          }
+        } 
+
         mkdirp(path, function(err) { 
           fs.writeFile(
-            path + '/' + type + '.json',
+            destFilename,
             body,
             cb
           )
 
-          console.log(body);
+          if (depth <= 5) {
+            let more = 
+              json.query.categorymembers.map(
+                (cat) => cat.title
+              ).map(
+                (title) => 
+                  _.partial(
+                    get,
+                    depth + 1,
+                    'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=' + title + '&cmlimit=500&cmtype=subcat',
+                    path + '/' + title.split(':')[1],
+                    'categories',
+                     5
+                  )
+                );
 
-          let more = 
-            json.query.categorymembers.map(
-              (cat) => cat.title
-            ).map(
-              (title) => 
-                _.partial(
-                  get,
-                  'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=' + title + '&cmlimit=500&cmtype=subcat',
-                  path + '/' + title.split(':')[1],
-                  'categories'
-                )
-            );
-
-           cb(more)
+             cb(more)
+           } else {
+             cb([]);
+           }
         });
       });  
     }
-  )
+  ) 
+  } catch (e) {
+    if (retries > 0) {
+      return cb([_.partial(get, depth, url, path, type, retries - 1)]);
+    } else {
+      return cb([]);
+    }
+  }
 }
 
 function save(type, path, data, cb) {
 }
 
 
-requests = [_.partial(get, url, 'data', 'categories')];
+requests = [_.partial(get, 0, url, 'data', 'categories', 5)];
 
 function recurse(requests1, requests2) {
   let requests = requests1.concat(requests2 || []);
   if (requests.length === 0) { return; }
 
-  next = requests.pop();
-console.log(next);
-  next(
-    _.partial(recurse, requests)
-  );
+  _.delay(() => {
+    if (requests1 && requests1.length > 0) {
+      next = requests1.pop();
+      next(
+        _.partial(recurse, requests1)
+      );
+    }
+
+    if (requests2 && requests2.length > 0) {
+      next = requests2.pop();
+      next(
+        _.partial(recurse, requests2)
+      );
+    }
+  }, 150);
 }
 
 recurse(requests);
