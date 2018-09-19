a5ec58e320e898eef725571f304d5a44cf65ddf5
Gary Sieling <garysieling@Garys-MacBook-Pro.local>
2018-09-19 08:26:19 -0400

    more data

--- articles.js
+++ articles.js
@@ -11,7 +11,6 @@ let mkdirp = require('mkdirp');
 
 let start = 'Main_topic_classifications'
 
-
 let walkSync = function(dir, filelist) {
   let files = fs.readdirSync(dir);
   filelist = filelist || [];
@@ -20,18 +19,11 @@ let walkSync = function(dir, filelist) {
       filelist = walkSync(dir + file + '/', filelist);
     }
     else {
-      filelist.push([file, dir.split('/').length]);
+      filelist.push([dir + file, dir.split('/').length - 2]);
     }
   });
   return filelist;
 };
-
-walkSync('data/').map(
-  (file) => {
-    console.log(file);
-  }
-)
-/*let url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:' + start + '&cmlimit=500'
 
 let retries = 50;
 function tryParse(text) {
@@ -42,33 +34,29 @@ function tryParse(text) {
   }
 }
 
-function get(depth, url, path, type, retries, cb) {
+walkSync('data/').map(
+  ([file, depth]) => {
+    if (depth > 0 && depth <= 3) {
+      console.log(file, depth);
+      const parts = file.split('/');
+      const cat = parts[parts.length - 2];
+      
+      let url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtype=page&cmtitle=Category:' + cat + '&cmlimit=500';
+      const path = file.substring(0, file.lastIndexOf('/'));
+      get(url, path, 'pages', 1, () => {});
+    }
+  }
+)
+
+
+function get(url, path, type, retries, cb) {
   try { 
     console.log(url);
     const destFilename = path + '/' + type + '.json';
 
-    if (depth > 3) {
-      return cb([]); 
-    }
-
     if (fs.existsSync(destFilename)) {
-      const json = JSON.parse(fs.readFileSync(destFilename, 'utf-8'));
-      let more = 
-        json.query.categorymembers.map(
-          (cat) => cat.title
-        ).map(
-          (title) => 
-            _.partial(
-              get,
-              depth + 1,
-              'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=' + title + '&cmlimit=500&cmtype=subcat',
-              path + '/' + title.split(':')[1],
-              'categories',
-              retries
-            )
-          );
-
-      return cb(more);
+      console.log('Cached ' + path);
+      return cb([]);
     }
      
   let req = https.get(
@@ -86,7 +74,7 @@ function get(depth, url, path, type, retries, cb) {
        
         if (body.length === 0 || _.get(json, ['query', 'categorymembers'], '') === '') {
           if (retries > 0) {
-            return cb([_.partial(get, depth, url, path, type, retries - 1)]);
+            return cb([_.partial(get, url, path, type, retries - 1)]);
           } else {
             return cb([]);
           }
@@ -106,7 +94,6 @@ function get(depth, url, path, type, retries, cb) {
                   (title) => 
                     _.partial(
                       get,
-                      depth + 1,
                       'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=' + title + '&cmlimit=500&cmtype=subcat',
                       path + '/' + title.split(':')[1],
                       'categories',
@@ -122,49 +109,14 @@ function get(depth, url, path, type, retries, cb) {
 
     req.on('error', () => {
       if (retries > 0) {
-         return cb([_.partial(get, depth, url, path, type, retries - 1)]);
+         return cb([_.partial(get, url, path, type, retries - 1)]);
       } else {
         return cb([]);
       }
     });
   } catch (e) {
-    if (retries > 0) {
-      return cb([_.partial(get, depth, url, path, type, retries - 1)]);
-    } else {
-      return cb([]);
-    }
   }
 }
 
-function save(type, path, data, cb) {
-}
 
-
-requests = [_.partial(get, 0, url, 'data', 'categories', retries)];
-
-function recurse(requests1, requests2) {
-  let requests = requests1.concat(requests2 || []);
-  if (requests.length === 0) { return; }
-
-  setTimeout( 
-    function() {
-    if (requests1 && requests1.length > 0) {
-      next = requests1.pop();
-      next(
-        _.partial(recurse, requests1)
-      );
-    }
-
-    if (requests2 && requests2.length > 0) {
-      next = requests2.pop();
-      
-      next(
-        _.partial(recurse, requests2)
-      );
-    }
-  }, 250);
-}
-
-recurse(requests);
-*/
 //'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtype=subcat&cmpagetitle=' + start + '&cmlimit=500'
